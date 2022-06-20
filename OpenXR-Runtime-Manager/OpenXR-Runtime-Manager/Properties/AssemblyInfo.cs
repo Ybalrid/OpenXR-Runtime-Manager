d7525c28f378bb61daae4a29d7a879667a810762
Ybalrid (Arthur Brainville) <ybalrid@ybalrid.info>
2022-06-20 14:44:15 +0200

    bump version to 0.1.5.0

--- OpenXR-Runtime-Manager/OpenXR-Runtime-Manager/Properties/AssemblyInfo.cs
+++ OpenXR-Runtime-Manager/OpenXR-Runtime-Manager/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OpenXR-Runtime-Manager")]
-[assembly: AssemblyCopyright("Copyright © 2021 Arthur Brainville (Ybalrid)")]
+[assembly: AssemblyCopyright("Copyright © 2021-2022 Arthur Brainville (Ybalrid)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.4.0")]
-[assembly: AssemblyFileVersion("0.1.4.0")]
+[assembly: AssemblyVersion("0.1.5.0")]
+[assembly: AssemblyFileVersion("0.1.5.0")]
